d51be8e859ac764806ea6e7ad1d8fbe5e60f5d0b
Varsha <varshabhadana93@gmail.com>
2022-09-12 22:10:39 +0200

    Implement random color generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import randomcolor from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState(' ');
+  const [hue, setHue] = useState('');
+  const [lightness, setLightness] = useState(' ');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
+        style={{
+          color: 'red',
+        }}
+      >
+        Random Color Generator
+      </h1>
+      <div
+        style={{
+          width: 400,
+          height: 400,
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: color,
+          borderRadius: '6px',
+          border: '5px solid',
+          borderColor: '#ffe6e6',
+        }}
+      >
+        {`Generated Color:${color}`}
+      </div>
+      <br />
+      <label>
+        Hue:
+        <input
+          style={{ padding: '5px', marginLeft: '10px' }}
+          value={hue}
+          onChange={(event) => setHue(event.target.value)}
+        />
+      </label>
+      <label style={{ marginLeft: '20px' }}>
+        Lightness:
+        <input
+          style={{ padding: '5px', marginLeft: '10px' }}
+          value={lightness}
+          onChange={(event) => setLightness(event.target.value)}
+        />
+      </label>
+      <br />
+      <button
+        style={{
+          borderRadius: '6px',
+          border: '1px solid',
+          borderColor: 'black',
+          color: '#ff8080',
+          fontSize: '15px',
+          padding: '20px 36px',
+          marginTop: '30px',
+          backgroundColor: '#ccf2ff',
+          fontWeight: 'bold',
+        }}
+        onClick={() => {
+          const newColor = randomcolor.randomColor({
+            luminosity: lightness,
+            hue: hue,
+          });
+          setColor(newColor);
+        }}
+      >
+        Generate
+      </button>
     </div>
   );
 }
