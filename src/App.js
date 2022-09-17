7aeafb57b6bdd735c5d13c4e6329891cdd3ea7d9
Varsha <varshabhadana93@gmail.com>
2022-09-18 00:30:48 +0200

    Test change

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   const [lightness, setLightness] = useState(' ');
 
   return (
-    <>
+    <div className="App">
       <h1
         style={{
           color: 'red',
@@ -52,7 +52,7 @@ function App() {
           borderColor: '#ffe6e6',
         }}
       >
-        <div>Generated Color: {color}</div>
+        <div> Generated Color: {color}</div>
       </div>
       <div css={hueStyle}>
         {/* selector for Hue */}
@@ -106,14 +106,13 @@ function App() {
         <button
           css={buttonStyle}
           onClick={() => {
-            const newColor = randomcolor();
-            setColor(newColor);
+            setColor(randomcolor());
           }}
         >
           Generate
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
